398d7325df39e04cf82d9ec7d25e010454a0ae04
unknown <you@example.com>
2022-09-15 17:00:38 +0530

    practise dao programs

---
+++ Hi.java
@@ -0,0 +1,8 @@
+class Hi
+{
+public static void main(String[] args)
+{
+System.out.println("Wish you happy birthday have a wonderful year ahead hope all your dreams come true stay blessed.... ");
+
+}
+}
